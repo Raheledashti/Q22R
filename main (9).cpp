7efa2d2cdd97a56346b14f28a05adb6c6a1dc2da
Raheledashti <dashtirahele751@gmail.com>
2024-06-07 03:07:50 +0330

    Add files via upload

---
+++ main (9).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,sum;
+    cout<<"a:";cin>>a;
+    cout<<"b";cin>>b;
+    cout<<"c";cin>>c;
+    cout<<"d";cin>>d;
+    cout<<"e";cin>>e;
+    sum=a+b+c+d+c;
+    cout<<"sum/5";
+    cout<<"bigger sum";
+    return 0;
+}
